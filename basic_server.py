0db80d7ed1939ada7163037007548840994ecc72
carsongrogg <carson.grogg@automox.com>
2024-12-16 15:40:48 -0700

    Add basic_server.py

---
+++ basic_server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import random
+import time
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class AutomoxBackend(Resource):
+    def get(self):
+        r = random.randint(1, 100)
+        if r < 80:
+            return {"Message": "Good checkin"}, 200
+        elif r < 90:
+            return {"Message": "Bad request"}, 400
+        time.sleep(random.random())
+        return {"Message": "Server error"}, 500
+
+
+api.add_resource(AutomoxBackend, '/v1/checkin')
+
+if __name__ == "__main__":
+    app.run(port=5001, host='0.0.0.0')
